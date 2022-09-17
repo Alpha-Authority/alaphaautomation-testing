e5ce3139963460567d045083933c673e6c8a79ca
OfficialRBXAA <official.alphaauthority@gmail.com>
2022-09-16 23:36:37 -0500

    Changed Embeds to EmbedBuilders

--- System/Client/V14_Noblox_Modules/profile.js
+++ System/Client/V14_Noblox_Modules/profile.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const axios = require("axios");
 const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('profile')
@@ -77,24 +78,30 @@ module.exports = {
                 .then(function (response){
                     console.log(response.data)
                     if (response.data.data.length == 0){
-                        var infoEmbed = {
-                            "description": `User **${interactionStringArg}** doesn't exist!`,
-                            "color": 16073282,
-                            "author": {
-                              "name": interaction.client.user.username,
-                              "icon_url": interaction.client.user.displayAvatarURL({ format: "png", dynamic: true })
-                            },
-                            "footer": {
-                              "text": interaction.guild.name,
-                              "icon_url": interaction.guild.iconURL({ format: "png", dynamic: true })
-                            },
-                            "timestamp": new Date(),
-                        }
+                        const embed = new EmbedBuilder()
+                          .setColor(16073282)
+                          .setAuthor({ name: interaction.client.user.username, iconURL: interaction.client.user.displayAvatarURL({ format: "png", dynamic: true }) })
+                          .setDescription(`User **${interactionStringArg}** doesn't exist!`)
+                          .setTimestamp()
+                          .setFooter({ text: interaction.guild.name, iconURL: interaction.guild.iconURL({ format: "png", dynamic: true })})
+                        //var infoEmbed = {
+                        //    "description": `User **${interactionStringArg}** doesn't exist!`,
+                        //    "color": 16073282,
+                        //    "author": {
+                        //      "name": interaction.client.user.username,
+                        //      "icon_url": interaction.client.user.displayAvatarURL({ format: "png", dynamic: true })
+                        //    },
+                        //    "footer": {
+                        //      "text": interaction.guild.name,
+                        //      "icon_url": interaction.guild.iconURL({ format: "png", dynamic: true })
+                        //    },
+                        //    "timestamp": new Date(),
+                        //}
                         //var badEmbed = new Discord.EmbedBuilder()
                             //.setColor(0xf54242)
                             //.setDescription(`User **${interactionStringArg}** doesn't exist!`)
-                        console.log(infoEmbed)
-                        return interaction.reply({ embeds: [infoEmbed]});
+                        console.log(embed)
+                        return interaction.reply({ embeds: [embed]});
                     }else{
                         rblx_username = response.data.data[0].name;
                         rblx_id = response.data.data[0].id;
@@ -117,25 +124,31 @@ module.exports = {
                     .then(function (response){
                         console.log(response.data)
                         if (response.data.data.length == 0){
-                            var infoEmbed = {
-                                "description": `User **${interactionStringArg}** doesn't exist!`,
-                                "color": 16073282,
-                                "author": {
-                                  "name": interaction.client.user.username,
-                                  "icon_url": interaction.client.user.displayAvatarURL({ format: "png", dynamic: true })
-                                },
-                                "footer": {
-                                  "text": interaction.guild.name,
-                                  "icon_url": interaction.guild.iconURL({ format: "png", dynamic: true })
-                                },
-                                "timestamp": new Date(),
-                              }
+                            const embed = new EmbedBuilder()
+                                .setColor(16073282)
+                                .setAuthor({ name: interaction.client.user.username, iconURL: interaction.client.user.displayAvatarURL({ format: "png", dynamic: true }) })
+                                .setDescription(`User **${interactionStringArg}** doesn't exist!`)
+                                .setTimestamp()
+                                .setFooter({ text: interaction.guild.name, iconURL: interaction.guild.iconURL({ format: "png", dynamic: true })})
+                            //var infoEmbed = {
+                            //    "description": `User **${interactionStringArg}** doesn't exist!`,
+                            //    "color": 16073282,
+                            //    "author": {
+                            //      "name": interaction.client.user.username,
+                            //      "icon_url": interaction.client.user.displayAvatarURL({ format: "png", dynamic: true })
+                            //    },
+                            //    "footer": {
+                            //      "text": interaction.guild.name,
+                            //      "icon_url": interaction.guild.iconURL({ format: "png", dynamic: true })
+                            //    },
+                            //    "timestamp": new Date(),
+                            //  }
                             //var badEmbed = new Discord.MessageEmbed()
                                 //.setColor(0xf54242)
                                 //.setDescription(`User **${roblox}** doesn't exist!`)
                             
-                            console.log(infoEmbed)
-                            return interaction.reply({ embeds: [infoEmbed] });
+                            console.log(embed)
+                            return interaction.reply({ embeds: [embed] });
                         }else{
                             rblx_username = response.data.data[0].name;
                             rblx_id = response.data.data[0].id;
@@ -176,21 +189,29 @@ module.exports = {
                                 noblox.getRankNameInGroup(790907, rblx_id)
                                 //axios.get(`https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${userid}&size=180x180&format=Png`)
                                     .then(function (rankid) {
-                                        var infoEmbed = {
-                                            "title": `${rblx_username}'s Profile`,
-                                            "description": `Username: ${rblx_username}\nXP: ${current_xp}\nRank: ${rankid}`,
-                                            "url": `https://www.roblox.com/users/${rblx_id}/profile`,
-                                            "color": 16747520,
-                                            "author": {
-                                              "name": interaction.client.user.username,
-                                              "icon_url": interaction.client.user.displayAvatarURL({ format: "png", dynamic: true })
-                                            },
-                                            "footer": {
-                                              "text": interaction.guild.name,
-                                              "icon_url": interaction.guild.iconURL({ format: "png", dynamic: true })
-                                            },
-                                            "timestamp": new Date(),
-                                          }
+                                        const embed = new EmbedBuilder()
+                                            .setColor(16747520)
+                                            .setTitle(`${rblx_username}'s Profile`)
+                                            .setURL(`https://www.roblox.com/users/${rblx_id}/profile`)
+                                            .setAuthor({ name: interaction.client.user.username, iconURL: interaction.client.user.displayAvatarURL({ format: "png", dynamic: true }) })
+                                            .setDescription(`Username: ${rblx_username}\nXP: ${current_xp}\nRank: ${rankid}`)
+                                            .setTimestamp()
+                                            .setFooter({ text: interaction.guild.name, iconURL: interaction.guild.iconURL({ format: "png", dynamic: true })})
+                                        //var infoEmbed = {
+                                        //    "title": `${rblx_username}'s Profile`,
+                                        //    "description": `Username: ${rblx_username}\nXP: ${current_xp}\nRank: ${rankid}`,
+                                        //    "url": `https://www.roblox.com/users/${rblx_id}/profile`,
+                                        //    "color": 16747520,
+                                        //    "author": {
+                                        //      "name": interaction.client.user.username,
+                                        //      "icon_url": interaction.client.user.displayAvatarURL({ format: "png", dynamic: true })
+                                        //    },
+                                        //    "footer": {
+                                        //      "text": interaction.guild.name,
+                                        //      "icon_url": interaction.guild.iconURL({ format: "png", dynamic: true })
+                                        //    },
+                                        //    "timestamp": new Date(),
+                                        //  }
                                         //var infoEmbed = new Discord.MessageEmbed()
                                         //    .setColor(0xff8c00)
                                         //    .setTitle(`${rblx_username}'s Profile`)
@@ -199,7 +220,7 @@ module.exports = {
                                             //.setThumbnail(response);
 
                                         // return embed
-                                        return interaction.reply( { embeds: [infoEmbed] } )
+                                        return interaction.reply( { embeds: [embed] } )
                                     })
                                     .catch(error => console.log(error));
                             }else{
@@ -207,24 +228,33 @@ module.exports = {
                                 noblox.getRankNameInGroup(790907, rblx_id)
                                     //axios.get(`https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${userid}&size=180x180&format=Png`)
                                     .then(function (rankid) {
-                                        var infoEmbed = {
-                                            "title": `${rblx_username}'s Profile`,
-                                            "description": `Username: ${rblx_username}\nXP: ${current_xp}\nRank: ${rankid}`,
-                                            "url": `https://www.roblox.com/users/${rblx_id}/profile`,
-                                            "color": 16747520,
-                                            "author": {
-                                              "name": interaction.client.user.username,
-                                              "icon_url": interaction.client.user.displayAvatarURL({ format: "png", dynamic: true })
-                                            },
-                                            "footer": {
-                                              "text": interaction.guild.name,
-                                              "icon_url": interaction.guild.iconURL({ format: "png", dynamic: true })
-                                            },
-                                            "timestamp": new Date(),
-                                            "thumbnail": {
-                                                "url": thumbnail
-                                              }
-                                          }
+                                        const embed = new EmbedBuilder()
+                                              .setColor(16747520)
+                                              .setTitle(`${rblx_username}'s Profile`)
+                                              .setURL(`https://www.roblox.com/users/${rblx_id}/profile`)
+                                              .setAuthor({ name: interaction.client.user.username, iconURL: interaction.client.user.displayAvatarURL({ format: "png", dynamic: true }) })
+                                              .setDescription(`Username: ${rblx_username}\nXP: ${current_xp}\nRank: ${rankid}`)
+                                              .setThumbnail(thumbnail)
+                                              .setTimestamp()
+                                              .setFooter({ text: interaction.guild.name, iconURL: interaction.guild.iconURL({ format: "png", dynamic: true })})
+                                        //var infoEmbed = {
+                                        //    "title": `${rblx_username}'s Profile`,
+                                        //    "description": `Username: ${rblx_username}\nXP: ${current_xp}\nRank: ${rankid}`,
+                                        //    "url": `https://www.roblox.com/users/${rblx_id}/profile`,
+                                        //    "color": 16747520,
+                                        //    "author": {
+                                        //      "name": interaction.client.user.username,
+                                        //      "icon_url": interaction.client.user.displayAvatarURL({ format: "png", dynamic: true })
+                                        //    },
+                                        //    "footer": {
+                                        //      "text": interaction.guild.name,
+                                        //      "icon_url": interaction.guild.iconURL({ format: "png", dynamic: true })
+                                        //    },
+                                        //    "timestamp": new Date(),
+                                        //    "thumbnail": {
+                                        //        "url": thumbnail
+                                        //      }
+                                        //  }
                                         //var infoEmbed = new Discord.MessageEmbed()
                                         //    .setColor(0xff8c00)
                                         //    .setTitle(`${rblx_username}'s Profile`)
@@ -233,7 +263,7 @@ module.exports = {
                                         //    .setThumbnail(thumbnail);
 
                                         // return embedinteraction.guild.iconURL({ format: "png", dynamic: true }
-                                        return interaction.reply( {embeds: [infoEmbed] } )
+                                        return interaction.reply( {embeds: [embed] } )
                                     })
                                     .catch(error => console.log(error));
                             }
