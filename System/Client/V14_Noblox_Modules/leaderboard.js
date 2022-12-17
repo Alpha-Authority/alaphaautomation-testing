3e456ae3e7851f2d10d16ec6c73b1404a8a6311f
OfficialRBXAA <official.alphaauthority@gmail.com>
2022-12-16 21:30:34 -0600

    XP Leaderboard.js

---
+++ System/Client/V14_Noblox_Modules/leaderboard.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const axios = require("axios");
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
+const { query } = require('express');
+const admin = require("firebase-admin");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('leaderboard')
+        .setDescription(`View a the point leaderboard.`)
+        .addStringOption(option =>
+            option.setName('username')
+                .setDescription(`View a user's group profile.`)
+                .setRequired(false)),
+	async execute(interaction, noblox) {
+        var db = admin.database();
+        const userdata = []
+        //var ref = db.ref('points').child('groups').child('Alpha Authority').child('users')
+        //ref.orderByChild('xp').on('value', (querySnapshot) => {
+        //    console.log()
+        //})
+    }
+}
