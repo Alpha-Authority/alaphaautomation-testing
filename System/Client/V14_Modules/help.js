98dae898fb10efd60de6ee9d90f40c54fac3f438
Scrippy <50034413+Scrippy@users.noreply.github.com>
2022-12-23 20:17:21 -0600

    Redid help.js

--- System/Client/V14_Modules/help.js
+++ System/Client/V14_Modules/help.js
@@ -3,33 +3,22 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
-        .setDescription(`Command information.`),
+        .setDescription('Gives information about the bot.'),
 	async execute(interaction) {
-        const data = [];
-        const { commands } = interaction.client.commands;
-        data.push('Here\'s a list of all my commands:');
-        data.push('\`\`' + commands.map(command => command.commandName).join('``, ``') + '\`\`');
-        //data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
-
-        //return message.channel.send(data, { split: true })
-        var x;
-        var y = ``;
-        for (x of data) {
-            y = y + `\n` + x
-        }
         const embedAA = {
-            "author": {
-                "name": interaction.client.user.username,
-                "icon_url": interaction.client.user.displayAvatarURL({ format: "png", dynamic: true })
-            },
-            color: 0x0099ff,
-            "footer": {
-                "text": interaction.guild.name,
-                "icon_url":  interaction.guild.iconURL({ format: "png", dynamic: true })
-            },
-            "description": y,
-            timestamp: new Date()
+                "title": "Alapha Here! >:3",
+                "author": {
+                    "name": interaction.client.user.username,
+                    "icon_url": interaction.client.user.displayAvatarURL({ format: "png", dynamic: true })
+                },
+                color: 0x0099ff,
+                "footer": {
+                    "text": interaction.guild.name,
+                    "icon_url": interaction.guild.iconURL({ format: "png", dynamic: true })
+                },
+                "description": `Hiya! I\'m a *multi-purple* discord bot, promptly set for serving the Alaphan community. I provide security, entertainment, and utility services for the Alaphan communities.\n\n You can view more of my commands via \`\`/commands\`\``,
+                timestamp: new Date()
         }
-        await interaction.reply({ embeds: [ embedAA ] });
-        }
-}
+		await interaction.reply({ embeds: [ embedAA ] });
+    }
+};
