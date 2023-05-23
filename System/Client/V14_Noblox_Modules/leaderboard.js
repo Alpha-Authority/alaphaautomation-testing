7b039c85f40b3d7580ca735d48b48a3941ab816a
OfficialRBXAA <official.alphaauthority@gmail.com>
2023-05-23 04:37:08 -0500

    First working leaderboard.js
    Frontend and Backend Support! Uses previous functions from the command.js module.
    
    Co-Authored-By: Scrippy <50034413+Scrippy@users.noreply.github.com>

--- System/Client/V14_Noblox_Modules/leaderboard.js
+++ System/Client/V14_Noblox_Modules/leaderboard.js
@@ -17,10 +17,44 @@ module.exports = {
         },
 	async execute(interaction, noblox) {
         var db = admin.database();
-        const userdata = []
-        //var ref = db.ref('points').child('groups').child('Alpha Authority').child('users')
-        //ref.orderByChild('xp').on('value', (querySnapshot) => {
-        //    console.log()
-        //})
+        //const userdata = [] -- Old Table
+        const thisObjectTable = []
+        thisObjectTable.push(`Leaderboard`)
+        let ig = 0
+        var ref = db.ref('points').child('groups').child('Alpha Authority').child('users')
+        ref.orderByChild('xp').limitToFirst(10).on('value', (querySnapshot) => {
+
+            querySnapshot.forEach((thisObject) => {
+                ig = ig + 1
+                console.log(thisObject.key, thisObject.val())
+                thisObjectTable.push(thisObject.key, ` - `, `${thisObject.val().xp}`)
+                if (ig == 10){
+                    LogthisObjectTable(thisObjectTable)
+                }
+            });
+        });
+        async function LogthisObjectTable(userdata){
+            var x;
+            var y = ``;
+            for (x of userdata) {
+                y = y + `\n` + x
+            }
+            console.log(y)
+            const embedAA = {
+                "author": {
+                    "name": interaction.client.user.username,
+                    "icon_url": interaction.client.user.displayAvatarURL({ format: "png", dynamic: true })
+                },
+                color: 0x0099ff,
+                "footer": {
+                    "text": interaction.guild.name,
+                    "icon_url":  interaction.guild.iconURL({ format: "png", dynamic: true })
+                },
+                "description": y,
+                timestamp: new Date()
+            }
+            return interaction.reply({ embeds: [ embedAA ] });
+        }
+
     }
 }
