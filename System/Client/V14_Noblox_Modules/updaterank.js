a77ec457ad888ff3a0839acffcf3423f084e3730
OfficialRBXAA <official.alphaauthority@gmail.com>
2023-05-23 03:50:13 -0500

    /updateranks V14 AAT Command [Working]
    This should be the working figure, as tested via our Trello data requirements and for our Demo videos on our YouTube channel.

--- System/Client/V14_Noblox_Modules/updaterank.js
+++ System/Client/V14_Noblox_Modules/updaterank.js
@@ -21,26 +21,97 @@ module.exports = {
     		return interaction.reply({ content: `Sorry ${message.author}, but only the owners can run that command!` }).then(message => message.delete({timeout: 5000, reason: "delete"}));
     	}
         
-        function isAuthorized(){
+        async function isAuthorized(){
             var db = admin.database();
             const userdata = []
             var ref = db.ref('points').child('groups').child('Alpha Authority').child('users')
             ref.once('value', (snapshot) => {
                 snapshot.forEach((childSnapshot) => {
-                    setTimeout(function timer() {
-                        var childKey = childSnapshot.key;
-                        var childData = childSnapshot.val();
-                        console.log(childKey, childData)
-                        const rankId = noblox.getRankInGroup(790907, Number(childKey))
+                    var childKey = childSnapshot.key;
+                    var childData = childSnapshot.val();
+                    console.log(childKey, childData)
+                    userdata.push({childKey, childData})
+                })
+                checkRanks(userdata)
+            })
+        }
+        async function checkRanks(userData){
+            console.log(userData)
+            for (let i = 0; i < userData.length; i++) {
+                setTimeout(async function timer() {
+                    try {
+                        const rankId = await noblox.getRankInGroup(790907, Number(userData[i].childKey))
                         console.log(rankId)
-                        if (rankId == (2 || 91 || 92 || 93 || 94 || 95 || 96 || 97)) {
-                            console.log(childKey, rankId)
+                        if (rankId == 2 || rankId == 3 || rankId == 91 || rankId == 92 || rankId == 93 || rankId == 94 || rankId == 95 || rankId == 96 || rankId == 97) {
+                            try {
+                                if ((Number(userData[i].childData.xp) >= 1) && (Number(userData[i].childData.xp) < 5)){
+                                    if (rankId !== 91) {
+                                        noblox.setRank(790907, Number(userData[i].childKey), 91)
+                                        console.log(`Set rank for ${userData[i].childKey}`)
+                                    } else {
+                                        console.log(`${userData[i].childKey} is alrready in the correct rank.`)
+                                    }
+                                }
+                                if ((Number(userData[i].childData.xp) >= 5) && (Number(userData[i].childData.xp) < 10)){
+                                    if (rankId !== 92) {
+                                        noblox.setRank(790907, Number(userData[i].childKey), 92)
+                                        console.log(`Set rank for ${userData[i].childKey}`)
+                                    } else {
+                                        console.log(`${userData[i].childKey} is alrready in the correct rank.`)
+                                    }
+                                }
+                                if ((Number(userData[i].childData.xp) >= 10) && (Number(userData[i].childData.xp) < 25)){
+                                    if (rankId !== 93) {
+                                        noblox.setRank(790907, Number(userData[i].childKey), 93)
+                                        console.log(`Set rank for ${userData[i].childKey}`)
+                                    } else {
+                                        console.log(`${userData[i].childKey} is alrready in the correct rank.`)
+                                    }
+                                }
+                                if ((Number(userData[i].childData.xp) >= 25) && (Number(userData[i].childData.xp) < 50)){
+                                    if (rankId !== 94) {
+                                        noblox.setRank(790907, Number(userData[i].childKey), 94)
+                                        console.log(`Set rank for ${userData[i].childKey}`)
+                                    } else {
+                                        console.log(`${userData[i].childKey} is alrready in the correct rank.`)
+                                    }
+                                }
+                                if ((Number(userData[i].childData.xp) >= 50) && (Number(userData[i].childData.xp) < 75)){
+                                    if (rankId !== 95) {
+                                        noblox.setRank(790907, Number(userData[i].childKey), 95)
+                                        console.log(`Set rank for ${userData[i].childKey}`)
+                                    } else {
+                                        console.log(`${userData[i].childKey} is alrready in the correct rank.`)
+                                    }
+                                }
+                                if ((Number(userData[i].childData.xp) >= 75) && (Number(userData[i].childData.xp) < 100)){
+                                    if (rankId !== 96) {
+                                        noblox.setRank(790907, Number(userData[i].childKey), 96)
+                                        console.log(`Set rank for ${userData[i].childKey}`)
+                                    } else {
+                                        console.log(`${userData[i].childKey} is alrready in the correct rank.`)
+                                    }
+                                }
+                                if (Number(userData[i].childData.xp) >= 100){
+                                    if (rankId !== 97) {
+                                        noblox.setRank(790907, Number(userData[i].childKey), 97)
+                                        console.log(`Set rank for ${userData[i].childKey}`)
+                                    } else {
+                                        console.log(`${userData[i].childKey} is alrready in the correct rank.`)
+                                    }
+                                }
+                            } catch {
+                                console.log(`Error setting rank for ${userData[i].childKey}, data ${userData[i].childData.xp}`)
+                            }
                         }
-                    // ...
-
-                    }, 500);
-                });
-            });
+                    } catch {
+                        console.log(`Error getting rank for ${userData[i].childKey}`)
+                    }
+                }, i * 7500);                         
+            }
+            setTimeout(function timer() {
+                console.log('Starting...')
+             }, userData.length * 1000 + 1000)
         }
     }
 }
