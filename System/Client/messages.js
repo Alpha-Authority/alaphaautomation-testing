9cdbf8b8c6127c7ab9c778d983aab7cfaf7f7dec
Boody <bodebi639@gmail.com>
2023-05-26 21:40:10 +0300

    Syntax improvements in messages.js

--- System/Client/messages.js
+++ System/Client/messages.js
@@ -1,32 +1,35 @@
 require("dotenv").config();
 function messages(client) {
-  client.on("messageCreate", (message) => {
-    if (message.author.bot) return;
-    if (message.content.toLowerCase() == "hi") {
-      //message.channel.send('hello there <@' + message.author.id + '>')
-    }
-    if (message.content.toLowerCase() == ":)") {
-      //message.channel.send('(:')
-    }
-    if (message.content.toLowerCase().indexOf("birthday") != -1) {
-      message.channel.send("Happy Birthday!!!");
-    }
-    if (message.content.toLowerCase().indexOf(process.env.M_HR) != -1) {
-      setTimeout(() => {
-        message.reply(
-          {
-            files: [
-              "https://cdn.discordapp.com/attachments/591048589908901928/996103534107967568/AAMemesOperationMetaburn.gif",
-            ],
-          },
-          1000
-        );
-      });
-      setTimeout(() => {
-        message.delete();
-      }, 2000);
-    }
-  });
+	client.on("messageCreate", (message) => {
+		if (message.author.bot) return;
+		if (message.content.toLowerCase() == "hi") {
+			//message.channel.send('hello there <@' + message.author.id + '>')
+		}
+		if (message.content.toLowerCase().includes("girl")) {
+		message.channel.send('Script is a girl!')
+		}
+	if (message.content.toLowerCase() == ":)") {
+			//message.channel.send('(:')
+		}
+		if (message.content.toLowerCase().includes("birthday")) {
+			message.channel.send("Happy Birthday!!!");
+		}
+		if (message.content.toLowerCase().indexOf(process.env.M_HR) != -1) {
+			setTimeout(() => {
+				message.reply(
+					{
+						files: [
+							"https://cdn.discordapp.com/attachments/591048589908901928/996103534107967568/AAMemesOperationMetaburn.gif",
+						],
+					},
+					1000
+				);
+			});
+			setTimeout(() => {
+				message.delete();
+			}, 2000);
+		}
+	});
 }
 
 module.exports = messages;
