61a8477f1cef41f468535cc54f28e4b471bc5498
Boody <bodebi639@gmail.com>
2023-05-28 01:08:47 +0300

    Variable 'msg' name changed to 'msgContent' in messages.js

--- System/Client/messages.js
+++ System/Client/messages.js
@@ -3,17 +3,17 @@ require("dotenv").config();
 
 function messages(client) {
   client.on("messageCreate", (message) => {
-		const msg = message.content.toLowerCase();
+		const msgContent = message.content.toLowerCase();
     
 		if (message.author.bot) return;
 
-    if (msg.includes("girl")) {
+    if (msgContent.includes("girl")) {
       message.channel.send("Script is a girl!");
     }
-    if (msg.includes("birthday")) {
+    if (msgContent.includes("birthday")) {
       message.channel.send("Happy Birthday!!!");
     }
-    if (msg.split(" ").every((word) => banned.includes(word))) {
+    if (msgContent.split(" ").every((word) => banned.includes(word))) {
       message.channel.send({
         files: [
           "https://cdn.discordapp.com/attachments/591048589908901928/996103534107967568/AAMemesOperationMetaburn.gif",
