ee7e824a0ecbf6de873adb92a576f9d3bef044a6
Boody <bodebi639@gmail.com>
2023-05-31 06:11:05 +0300

    Removed unused line in messages.js

--- System/Client/messages.js
+++ System/Client/messages.js
@@ -1,11 +1,10 @@
 const banned = require("../../config/banned.json");
-require("dotenv").config();
 
 function messages(client) {
   client.on("messageCreate", async (message) => {
-		const msgContent = message.content.toLowerCase();
-    
-		if (await message.author.bot) return;
+    const msgContent = message.content.toLowerCase();
+
+    if (await message.author.bot) return;
 
     if (await msgContent.includes("girl")) {
       await message.channel.send("Script is a girl!");
@@ -13,13 +12,18 @@ function messages(client) {
     if (await msgContent.includes("birthday")) {
       await message.channel.send("Happy Birthday!!!");
     }
-    if (await msgContent.split(" ").every((word) => banned.includes(word))) {
-      await message.channel.send({
-        files: [
-          "https://cdn.discordapp.com/attachments/591048589908901928/996103534107967568/AAMemesOperationMetaburn.gif",
-        ],
-      });
-			await message.delete();
+    if (
+      await msgContent
+        .replace(/[^\w\s]/gi, "") // Replaces special characters
+        .split(" ")
+        .every((word = word) => banned.includes(word))
+    ) {
+      await message.delete();
+      // await message.channel.send({
+      // 	files: [
+      // 		"https://cdn.discordapp.com/attachments/591048589908901928/996103534107967568/AAMemesOperationMetaburn.gif",
+      // 	],
+      // });
     }
   });
 }
